6ce8e095888e044800d3a555baec81f9563710d7
MandiVamshi <Vamshi.ReddyMandi@cognizant.com>
2026-06-02 10:10:43 +0530

    updated toggle button

--- src/components/Catalog/CatalogHome.js
+++ src/components/Catalog/CatalogHome.js
@@ -1,7 +1,9 @@
 import { Link, Outlet, useNavigate } from 'react-router-dom';
+import { useState } from 'react';
 
 export default function CatalogHome() {
     const navigate = useNavigate();
+    const [isOpen, setIsOpen] = useState(false);  
 
     return (
         <div>
@@ -12,59 +14,58 @@ export default function CatalogHome() {
                         Catalog
                     </Link>
 
+
                     <button
                         className="navbar-toggler"
                         type="button"
-                        data-bs-toggle="collapse"
-                        data-bs-target="#catalogNavbar"
-                        aria-controls="catalogNavbar"
-                        aria-expanded="false"
+                        onClick={() => setIsOpen(!isOpen)}
                         aria-label="Toggle navigation"
                     >
                         <span className="navbar-toggler-icon"></span>
                     </button>
 
-                    <div className="collapse navbar-collapse" id="catalogNavbar">
+
+                    <div className={`collapse navbar-collapse ${isOpen ? "show" : ""}`}>
                         <ul className="navbar-nav me-auto">
 
                             <li className="nav-item">
-                                <Link className="nav-link" to="insert">
+                                <Link className="nav-link" to="insert" onClick={() => setIsOpen(false)}>
                                     Insert Catalog
                                 </Link>
                             </li>
 
                             <li className="nav-item">
-                                <Link className="nav-link" to="delete">
+                                <Link className="nav-link" to="delete" onClick={() => setIsOpen(false)}>
                                     Delete Catalog
                                 </Link>
                             </li>
 
                             <li className="nav-item">
-                                <Link className="nav-link" to="update">
+                                <Link className="nav-link" to="update" onClick={() => setIsOpen(false)}>
                                     Update Catalog
                                 </Link>
                             </li>
 
                             <li className="nav-item">
-                                <Link className="nav-link" to="getById">
+                                <Link className="nav-link" to="getById" onClick={() => setIsOpen(false)}>
                                     Get Catalog By Id
                                 </Link>
                             </li>
 
                             <li className="nav-item">
-                                <Link className="nav-link" to="getByProduct">
+                                <Link className="nav-link" to="getByProduct" onClick={() => setIsOpen(false)}>
                                     Get Catalogs By Product
                                 </Link>
                             </li>
 
                             <li className="nav-item">
-                                <Link className="nav-link" to="getAll">
+                                <Link className="nav-link" to="getAll" onClick={() => setIsOpen(false)}>
                                     Get All Catalogs
                                 </Link>
                             </li>
 
                             <li className="nav-item">
-                                <Link className="nav-link" to="getAllCatalogsPaginated">
+                                <Link className="nav-link" to="getAllCatalogsPaginated" onClick={() => setIsOpen(false)}>
                                     Get All Catalogs Paginated
                                 </Link>
                             </li>
