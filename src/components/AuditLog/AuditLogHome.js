92d13a005c25a984b1a327de66565065ddca5dae
harinadh1405 <2483500@cognizant.com>
2026-06-03 12:32:32 +0530

    updated

--- src/components/AuditLog/AuditLogHome.js
+++ src/components/AuditLog/AuditLogHome.js
@@ -62,14 +62,14 @@ export default function AuditLogHome() {
                                         Dashboard
                                     </button>
                                 </li>
-                                <li className="nav-item">
+                                {/* <li className="nav-item">
                                     <button
                                         className="btn btn-outline-light btn-sm"
                                         onClick={() => { localStorage.clear(); navigate('/login'); }}
                                     >
                                         Logout
                                     </button>
-                                </li>
+                                </li> */}
                             </ul>
                         </div>
 
